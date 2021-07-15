6cbba0ab852811effc7b10dc19e957098659286e
Zarovniy Serhiy <zarovniyserhiy1991@gmail.com>
2021-07-15 15:49:04 +0200

    Working on new project
    SocialNetwork on ReactJS

---
+++ src/components/Header/header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <ul>
+                <li>Home</li>
+                <li>Music</li>
+                <li>Image</li>
+            </ul>
+        </div>
+    );
+}
+
+
+export default Header
