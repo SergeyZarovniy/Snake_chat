401a706bdaed7ad3281a0288cfaa75d531d5ce61
Zarovniy Serhiy <zarovniyserhiy1991@gmail.com>
2021-07-16 13:04:08 +0200

    Working with project

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-
+import React from 'react';
 import './App.css';
-import './components/Header/header';
-import Header from './components/Header/header';
-import './components/Technologies/technologies'
-import Technologies from './components/Technologies/technologies';
+
+
+
 
 
 
 const App = () => {
   return (
-    <div className="App">
-      <Header/>
-      <Technologies/>
-      
+    <div className="app-wrapper">
+      <header>
+        <img src='https://i.pinimg.com/originals/25/73/db/2573dbb83c5db2e5e55554f4a26b77bf.jpg'></img>
+      </header>
+              <nav>
+
+                  <div> My Profile </div>
+                  <div> Messages </div>
+                 <div> Music </div>
+                <div> Settings </div>
+
+             </nav>
+
+                   <div>
+                     Main Content
+                   </div>
+
     </div>
   );
 }
