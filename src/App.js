cee315b33b4640e994e3725c410dcea528482867
Zarovniy Serhiy <zarovniyserhiy1991@gmail.com>
2021-07-16 14:28:01 +0200

    Working on new proj

--- src/App.js
+++ src/App.js
@@ -9,20 +9,54 @@ import './App.css';
 const App = () => {
   return (
     <div className="app-wrapper">
-      <header>
-        <img src='https://i.pinimg.com/originals/25/73/db/2573dbb83c5db2e5e55554f4a26b77bf.jpg'></img>
+      <header className="header">
+        <img src='https://media.istockphoto.com/vectors/viper-snake-sport-logo-design-vector-id1255768797?k=6&m=1255768797&s=170667a&w=0&h=Ai5arIpjB0eoF6Wrio42f2zG3OVnHQDHecAnhXpUUwY='></img>
+       
       </header>
-              <nav>
+              <nav className="nav">
 
-                  <div> My Profile </div>
-                  <div> Messages </div>
-                 <div> Music </div>
-                <div> Settings </div>
+                  <div> 
+                    <a> My Profile</a> 
+                     </div>
+
+                     <div> 
+                    <a> Messages </a> 
+                     </div>
+
+                     <div> 
+                    <a> News </a> 
+                     </div>
+
+                     <div> 
+                    <a> Music </a> 
+                     </div>
+
+                     <div> 
+                    <a> Settings </a> 
+                     </div>
 
              </nav>
 
-                   <div>
-                     Main Content
+                   <div className="content">
+                     <div>
+                       <img src='https://www.rentcafe.com/blog/wp-content/uploads/sites/5/2020/06/minimalism-save-money-featured.png'></img>
+                     </div>
+                     <div>
+                       ava + description
+                     </div>
+                     <div>
+                       My posts
+                       <div>
+                         New post
+                       </div>
+                     </div>
+                     <div>
+                       post 1 
+                     </div>
+                     <div>
+                       post 2
+                     </div>
+
                    </div>
 
     </div>
